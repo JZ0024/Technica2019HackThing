f12795a9718eb0d7a4387a34cbb8510ed7edc741
JZ0024 <jessica.zhang123@gmail.com>
2019-11-09 15:22:26 -0500

    Jessica Zhang -- Added basic SQLAlchemy framework

--- Database.py
+++ Database.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import sqlalchemy as sq
+
+class Equipment(Base):
+    __tablename__ = 'equipment'
+
+    name = Column(String, primary_key=True)
+    count = Colum(Integer)
+
+    def __repr__(self):
+        return "Equipment: %s" %self.name
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+def get_name():
+    pass
+
+def enter_equip(tag_name, ownership, description):
+    pass
