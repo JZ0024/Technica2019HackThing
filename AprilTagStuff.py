f89b2840cd57d400b27b8167d7a8b3f16c3736a8
JZ0024 <jessica.zhang123@gmail.com>
2019-11-10 01:30:52 -0500

    Jessica Zhang -- Basic CV/AprilTag Code

--- AprilTagStuff.py
+++ AprilTagStuff.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python3
+
+import cv2
+import apriltag
+import numpy as np
+
+
+def scan_image(i):
+    detector = apriltag.Detector()
+    dets = detector.detect(i)
+
+    return dets
+
+def take_picture()
+
+
+if __name__ == "__main__":
+    img = 'test2.png'
+    image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    dts = scan_image(image)
+    homography = None
+    print(dts)
+
+    for d in dts:
+        retval = image.copy()
+        matrix = d.corners
+        homography = cv2.polylines(image, [np.int32(matrix)], True, (255, 0, 0), 3)
+
+    if homography:
+        cv2.imshow("Homography", homography)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
